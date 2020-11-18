ebddc82f48850066c3b19545005264bfed296594
Lucio Anderlini <Lucio.Anderlini@cern.ch>
2020-11-18 13:04:36 +0100

    fixed typo in setup

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setuptools.setup (
   long_description = long_description,
   long_description_content_type = 'text/markdown', 
   url = 'https://github.com/landerlini/scorf',
-  packages = setuptools.find_package(),
+  packages = setuptools.find_packages(),
   classifiers = [
     'Programming Language :: Python :: 3',
     'License :: OSI Approved :: MIT License', 
